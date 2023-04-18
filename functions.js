661f2ed829d9a6c2c3e3dc9d772a2443316e9d8b
Brian Strickland <bstrick@gmail.com>
2023-04-18 11:32:04 -0400

    inital

---
+++ functions.js
@@ -0,0 +1,114 @@
+function generateQuestion1()
+{
+
+    const q1Table = "q1_table";
+    const q1BlockA = "q1_block_a";
+    const q1BlockB = "q1_block_b";
+    const q1Counter = "q1_counter";
+    const q1KeyPairs = getTablePairs();
+    const q1BlockAAns = "q1_block_a_ans";
+
+    buildTable(q1Table,q1KeyPairs);
+    setBlockValue(q1BlockA, getRandomFourBit());
+    setBlockValue(q1BlockB, getRandomFourBit());
+    setBlockValue(q1Counter, getRandomFourBit());
+    
+    var a = calculateDirectEncryption(getBlockValue(q1BlockA), q1KeyPairs);
+    var b = calculateDirectEncryption(getBlockValue(q1BlockB), q1KeyPairs);
+
+    setBlockValue(q1BlockAAns, a);
+    setBlockValue("q1_block_b_ans", b);
+
+}
+
+function setBlockValue(id, value)
+{
+    const mySpan = document.getElementById(id);
+
+    mySpan.textContent = value;
+}
+
+function getBlockValue(id)
+{
+    const mySpan = document.getElementById(id);
+
+    return mySpan.textContent;
+}
+
+function getRandomFourBit()
+{
+    // Generate a random integer between 0 and 15 (inclusive)
+    const randomInt = Math.floor(Math.random() * 16);
+ 
+    // Convert the integer to a 4-bit binary string
+    const encryptionKey = ("000" + randomInt.toString(2)).slice(-4);
+
+    return encryptionKey;
+}
+
+function buildTable(table, inputEncryptionPairs)
+{
+    const myTable = document.getElementById(table);
+
+    /*
+    for (const inputValue in inputEncryptionPairs) {
+        // Create a new row element
+        const row = myTable.insertRow();
+        // Create two new cell elements
+        const inputCell = row.insertCell();
+        const encryptionKeyCell = row.insertCell();
+        // Set the text content of the cells to the input value and encryption key, respectively
+        inputCell.textContent = inputValue;
+        encryptionKeyCell.textContent = inputEncryptionPairs[inputValue];
+      }
+      */
+      const inputRow = myTable.insertRow();
+     // Create the input value header cell
+const inputValueHeaderCell = inputRow.insertCell();
+inputValueHeaderCell.textContent = "Input, i.e. x";
+// Loop through the inputEncryptionPairs object and create a new cell for each input value
+for (const inputValue in inputEncryptionPairs) {
+  const cell = inputRow.insertCell();
+  cell.textContent = inputValue;
+}
+
+// Create the second row for encryption keys
+const encryptionKeyRow = myTable.insertRow();
+// Create the encryption key header cell
+const encryptionKeyHeaderCell = encryptionKeyRow.insertCell();
+encryptionKeyHeaderCell.textContent = "Encryption Output, i.e. E_K(x";
+// Loop through the inputEncryptionPairs object and create a new cell for each encryption key
+for (const inputValue in inputEncryptionPairs) {
+  const cell = encryptionKeyRow.insertCell();
+  cell.textContent = inputEncryptionPairs[inputValue];
+}
+}
+
+function getTablePairs()
+{
+     // Define an array of possible input values
+     const inputValues = ["0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"];
+     const inputEncryptionPairs = {};
+ 
+     // Loop through the input values array
+     for (let i = 0; i < inputValues.length; i++) {
+ 
+         // Convert the integer to a 4-bit binary string
+         const encryptionKey = getRandomFourBit();
+ 
+         inputEncryptionPairs[inputValues[i]] = encryptionKey
+     }
+ 
+     // Log the input values and encryption keys arrays to the console
+     console.log(inputValues);
+     console.log(inputEncryptionPairs);
+
+     return inputEncryptionPairs;
+}
+
+function calculateDirectEncryption(input, inputEncryptionPairs)
+{
+    console.log(input);
+    console.log(inputEncryptionPairs);
+    return inputEncryptionPairs[input];
+}
